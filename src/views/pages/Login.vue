e99e9ceb153bbb95e47158e24762e5a7c06b848a
David Valentin <david@mrnoow.com>
2019-02-14 11:36:03 +0100

    login display

--- src/views/pages/Login.vue
+++ src/views/pages/Login.vue
@@ -2,14 +2,12 @@
 
   <v-app id="inspire">
     <v-content>
-      <v-container fluid fill-height>
-        <v-layout align-center justify-center>
-          <v-flex xs12 sm8 md4>
-            <v-card class="elevation-12">
-              <v-toolbar dark color="orange">
-                <v-toolbar-title>Login form</v-toolbar-title>
-                <v-spacer></v-spacer>
-              </v-toolbar>
+      <v-container grid-list-md  fill-height>
+
+      <v-layout row wrap align-center justify-center>
+
+          <v-flex xs12 md6 class="px0">
+
               <v-card-text>
                 <v-form>
                   <v-text-field prepend-icon="person" name="login" label="Login" type="text" v-model="email" color="orange"></v-text-field>
@@ -24,7 +22,23 @@
               </v-card-actions>
                <v-divider light></v-divider>
 
-            </v-card>
+          </v-flex>
+          <v-flex xs12 md6 px0>
+
+              <v-card-text>
+                <v-form>
+                  <v-text-field prepend-icon="person" name="login" label="Login" type="text" v-model="email" color="orange"></v-text-field>
+                  <v-text-field id="password" prepend-icon="lock" name="password" label="Password" type="password" color="orange" v-model="password" ></v-text-field>
+
+                </v-form>
+              </v-card-text>
+              <v-card-actions class="pa-3">
+                <span>Don't have an account?  <a class="orange--text" @click="signup" >Sign up</a></span>
+                <v-spacer></v-spacer>
+                <v-btn dark color="orange "  @click="login">Login</v-btn>
+              </v-card-actions>
+               <v-divider light></v-divider>
+
           </v-flex>
         </v-layout>
       </v-container>
